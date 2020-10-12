e3787a713ff74351041b58e99b2ecd0b29e408f5
Th3OnlyC0D3R <nico.fengler@gmx.de>
2020-10-12 21:52:34 +0200

    add win10 2004 support

---
+++ Genslich/Genslich.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Genslich
+{
+    public static class Genslich
+    {
+        static Genslich()
+        {
+            Console.WriteLine("DLL MAIN (Only DLL_PROCESS_ATTACH) :D");
+        }
+    }
+}
